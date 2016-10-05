be2de5a297c7053ccfbf6ad428086d43fdbed4aa
victormelo <victorklbr@gmail.com>
2016-10-05 16:28:53 -0300

    update to the latest version

--- knn.py
+++ knn.py
@@ -3,27 +3,27 @@
 import numpy as np
 from scipy.io.arff import loadarff
 
-def knn(Xt, Y, X, k=3):
+def knn(Xt, Y, X, k):
     # input: Xt, amostras a serem classificadas
     #        Y, Rótulo das classes das amostras X de treinamento
     #        X, Amostras de treinamento
     #        k, n. de vizinhos do classificador k-NN
     # output: Ytest, labels previstas para as amostras Xt
     h, w = Xt.shape
-    Ytest = np.empty((h,), '|S15')
+    Ytest = np.empty((h,), 'S15')
 
     # para todos as h amostras de teste
     for i in range(h):
 
         # calculo da distância euclidiana de cada amostra de treino X
         # com a i amostra de teste
-        difference = (X - Xt[i]) ** 2
-        dist = np.sqrt(np.sum(difference, axis=1))
+        dist = np.sqrt(np.sum(np.power(X - Xt[i], 2), axis=1))
 
         # os k indices com menor distância
         k_indices = np.argsort(dist)[:k]
         labels = list(Y[k_indices])
         mode = max(labels, key=labels.count)
+
         Ytest[i] = mode
 
 
@@ -44,8 +44,8 @@ def convert_to_numpy_array(list_of_tuples):
 
 if __name__ == '__main__':
 
-    train, meta = loadarff(open('vehicle-train.arff', 'r'))
-    test, meta = loadarff(open('vehicle-test.arff', 'r'))
+    train, meta = loadarff(open('iris-train.arff', 'r'))
+    test, meta = loadarff(open('iris-test.arff', 'r'))
 
     # amostras de treinamento
     X = convert_to_numpy_array(train)
@@ -59,10 +59,9 @@ if __name__ == '__main__':
     # label que pertece as amostras de teste, usada para calcular a accuracy
     Yt = test['class']
 
-    # myYt_k3 são as classes previstas para k=3
-    myYt = knn(Xt, Y, X)
+    # myYt são as classes previstas para k=3
+    myYt = knn(Xt, Y, X, 3)
 
-    certos_k3 = (myYt == Yt).sum()
-    print 'Correct classified %d\n' % certos_k3
-    print 'Accuracy k=3: %.4f%%' % (certos_k3 / float(myYt.size) * 100)
-
+    certos = (myYt == Yt).sum()
+    print 'Correct classified %d\n' % certos
+    print 'Accuracy k=3: %.4f%%' % (certos / float(myYt.size) * 100)
